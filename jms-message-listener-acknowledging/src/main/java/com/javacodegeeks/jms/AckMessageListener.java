0cbda14ee93fa0e4a2115659a68a8d035ef08ba5
selvaraja_borngroup <selvaraja.savarimuthu@gmail.com>
2016-06-10 17:33:19 +0530

    Apache Active MQ using JMS.

---
+++ jms-message-listener-acknowledging/src/main/java/com/javacodegeeks/jms/AckMessageListener.java
@@ -0,0 +1,24 @@
+package com.javacodegeeks.jms;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+public class AckMessageListener implements MessageListener {
+	private boolean acknowledge;
+
+	public AckMessageListener(boolean acknowledge) {
+		this.acknowledge = acknowledge;
+	}
+
+	public void onMessage(Message message) {
+		if (acknowledge) {
+			try {
+				message.acknowledge();
+			} catch (JMSException e1) {
+				e1.printStackTrace();
+			}
+		}
+		System.out.println(message);
+	}
+}
---
+++ spring-apache-activemq/src/test/java/test/MessageProducerTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.roytuts.spring.jms.producer.MessageProducer;
+ 
+public class MessageProducerTest {
+ 
+	private MessageProducer messageProducer;
+ 
+	@SuppressWarnings("resource")
+	@Before
+	public void setUp() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"classpath:spring/apache-activemq-context.xml");
+		messageProducer = (MessageProducer) applicationContext
+				.getBean("messageProducer");
+	}
+ 
+	@Test
+	public void testSendMessageToDefaultDestination() {
+		messageProducer
+				.sendMessageToDefaultDestination("Send this message to default destination.");
+	}
+	
+}
