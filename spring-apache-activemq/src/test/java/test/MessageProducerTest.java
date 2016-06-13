c609fc5a27d4f9f4b0d4277c6931088a762982ae
selvaraja_borngroup <selvaraja.savarimuthu@gmail.com>
2016-06-13 15:05:13 +0530

    code refactoring.

--- spring-apache-activemq/src/test/java/test/MessageProducerTest.java
+++ spring-apache-activemq/src/test/java/test/MessageProducerTest.java
@@ -1,10 +1,5 @@
 package test;
  
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.roytuts.spring.jms.producer.MessageProducer;
  
 public class MessageProducerTest {
